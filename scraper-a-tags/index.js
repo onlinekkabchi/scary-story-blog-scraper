8b41792c4d7a160b0aa2e605132a9bd408de1ee7
hyjang0620 <hyjang@softhi.co.kr>
2024-02-11 18:41:07 +0900

    feat: 무한스크롤로 페이지 길이 5회 추가 후 태그 가져오기 성공

--- scraper-a-tags/index.js
+++ scraper-a-tags/index.js
@@ -1,17 +1,65 @@
 import axios from "axios";
 import jsdom from "jsdom";
 import express from "express";
+import webdriver, { By } from "selenium-webdriver";
 
 const { JSDOM } = jsdom;
 const app = express();
 const port = 3001;
+
+const url = "https://www.reddit.com/r/TwoSentenceHorror/top/";
+
+const findAllATags = async () => {
+  const driver = await new webdriver.Builder()
+    .forBrowser(webdriver.Browser.CHROME)
+    .build();
+
+  try {
+    await driver.get(url);
+    // await driver.actions(10, 10, 0, 5000);
+
+    let std = 0;
+
+    while (std < 5) {
+      let lastHeight = await driver.executeScript(
+        "return document.body.scrollHeight"
+      );
+
+      await driver.executeScript(
+        "window.scrollTo(0, document.body.scrollHeight);"
+      );
+
+      await driver.sleep(2000);
+
+      let newHeight = await driver.executeScript(
+        "return document.body.scrollHeight"
+      );
+
+      console.log(std);
+      console.log(lastHeight, newHeight);
+      std++;
+    }
+
+    const tags = await driver.findElements(By.css("a"));
+    // let taglength = 0;
+
+    for (let tag of tags) {
+      const href = await tag.getAttribute("href");
+      // console.log(tag.getAttribute("href"));
+      // href.includes("comment") ? taglength++ : "";
+    }
+  } catch (err) {
+    console.log(err);
+  } finally {
+  }
+};
 
 const main = async () => {
   let length = 0;
   let aTags = [];
   try {
     await axios
-      .get("https://www.reddit.com/r/TwoSentenceHorror/top/")
+      .get(url)
       .then(function (res) {
         const dom = new JSDOM(res.data);
         [...dom.window.document.querySelectorAll("a")].forEach(
@@ -29,7 +77,8 @@ const main = async () => {
 };
 
 app.get("/", (req, res) => {
-  main();
+  // main();
+  findAllATags();
   res.send("hi");
 });
 
