467f7cef3b379d5b19475642d365706d851e14b6
hyjang0620 <hyjang@softhi.co.kr>
2024-02-11 23:29:02 +0900

    feat: mongodb 연결

--- scraper-a-tags/index.js
+++ scraper-a-tags/index.js
@@ -1,47 +1,58 @@
 import axios from "axios";
 import jsdom from "jsdom";
 import express from "express";
+import dotenv from "dotenv";
 import webdriver, { By } from "selenium-webdriver";
+import { MongoClient } from "mongodb";
+
+dotenv.config();
 
 const { JSDOM } = jsdom;
 const app = express();
 const port = 3001;
 
 const url = "https://www.reddit.com/r/TwoSentenceHorror/top/";
+const uri = process.env.MONGO_URI;
 
-const findAllATags = async () => {
+const connectMongo = async () => {
+  const client = new MongoClient(uri);
+
+  try {
+    const database = client.db("sample_mflix");
+    const movies = database.collection("movies");
+    const query = { title: "Back to the Future" };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+  } finally {
+    await client.close();
+  }
+};
+
+const findAllATagsAfterInfiniteScroll = async () => {
   const driver = await new webdriver.Builder()
     .forBrowser(webdriver.Browser.CHROME)
     .build();
 
   try {
     await driver.get(url);
-    // await driver.actions(10, 10, 0, 5000);
 
     let std = 0;
 
     while (std < 5) {
-      let lastHeight = await driver.executeScript(
-        "return document.body.scrollHeight"
-      );
-
+      // let lastHeight = await driver.executeScript(
+      //   "return document.body.scrollHeight"
+      // );
       await driver.executeScript(
         "window.scrollTo(0, document.body.scrollHeight);"
       );
-
       await driver.sleep(2000);
 
-      let newHeight = await driver.executeScript(
-        "return document.body.scrollHeight"
-      );
-
-      console.log(std);
-      console.log(lastHeight, newHeight);
       std++;
     }
 
     const tags = await driver.findElements(By.css("a"));
-    // let taglength = 0;
+    const filterTags = [];
 
     for (let tag of tags) {
       const href = await tag.getAttribute("href");
@@ -54,31 +65,15 @@ const findAllATags = async () => {
   }
 };
 
-const main = async () => {
-  let length = 0;
-  let aTags = [];
-  try {
-    await axios
-      .get(url)
-      .then(function (res) {
-        const dom = new JSDOM(res.data);
-        [...dom.window.document.querySelectorAll("a")].forEach(
-          (el) =>
-            el.href.includes("comment") && aTags.push(el.outerHTML) && length++
-        );
-      })
-      .then(function () {
-        console.log(aTags);
-        console.log(length);
-      });
-  } catch (err) {
-    console.log(err);
-  }
-};
+app.get("/connect", (req, res) => {
+  connectMongo();
+});
+
+app.get("/tags", (req, res) => {
+  findAllATagsAfterInfiniteScroll();
+});
 
 app.get("/", (req, res) => {
-  // main();
-  findAllATags();
   res.send("hi");
 });
 
